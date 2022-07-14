b40ac2f6ad114f672d6328965b02941330959f2b
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-07-14 19:31:04 -0400

    commit tree e314bf26becff8b12680f22eb5aef87cf1756614

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+name TEXT,
+id INTEGER PRIMARY KEY,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Mr.Chocolate", 2, "M", "red", "nice", 0);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Rowdy", 4, "F", "blue", "mean", 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Tabitha", 9, "M", "pink", "kind", 0);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Sergeant Brown", 5, "F", "green", "wise", 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Melissa", 1, "F", "yellow", "aloof", 0);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Grinch", 10, "F", "brown", "weird", 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES("Wendy", 24, "M", "auburn", "angry", 1);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES(NULL, 20, "M", "purple", "unknown", 0);
